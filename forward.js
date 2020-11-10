41b7a50f412afa11123e178055a93c4b5f687063
sorentorp <soren.torp@gmail.com>
2020-11-10 14:12:58 +0100

    tilføjet Google Drive direkte link

--- forward.js
+++ forward.js
@@ -10,10 +10,12 @@ switch(hostname) {
     case 'broker.unilogin.dk':
        handleUnilogin();
         break;
+    case 'drive.google.com':
+       handleDrive();
+        break;        
     default:
         break;   
 }
-
 
 function handleTabulex() {
     const buttons = document.getElementsByTagName("button"); // getElementsById hvis knappen har et id
@@ -21,6 +23,31 @@ function handleTabulex() {
     for (const button of buttons) {
         inspectTabulexButton(button);
     }
+}
+
+function handleDrive() {
+    window.setInterval(() => {
+     let frame = Array.from(document.getElementsByTagName('iframe')).find((x) => x.src.includes('/sharing/driveshare'));
+    
+        if(!frame) return; 
+        if(frame.contentDocument.querySelectorAll('h2').length != 4) return; 
+
+        let title = frame.contentDocument.querySelectorAll('h2')[3];
+        let url = Array.from(frame.contentDocument.querySelectorAll('input[type="text"]')).find((x) => x.value.includes('drive.google.com'))
+
+        if(!url) return; 
+
+        url = url.value;
+
+        let fid = url.match(/https:\/\/drive.google\.com\/file\/d\/(.*)\/view(.*)/)[1];
+        
+        let downloadLink = 'https://drive.google.com/uc?id=' + fid ;
+
+        if(title.innerHTML.includes('Direkte link')) return; 
+
+        title.innerHTML = title.innerHTML + '<a href="' + downloadLink + ' "target="_blank"" " style="font-size: 0.8rem; margin-left: 20px; target-new: window" >Direkte link</a>'
+
+    }, 1000)
 }
               
 function inspectTabulexButton(button) {
